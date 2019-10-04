03cea0eb8734112ccc5f9fe5d1327a8622a8ed07
“renexon” <mujahidul15@gmail.com>
2019-10-03 22:34:09 -0400

    project1

--- src/Constructor.java
+++
@@ -1,8 +0,0 @@
-
-public class Constructor {
-	public static void main (String[] args) {
-		Practice practice =new Practice();
-		System.out.println(practice.sub2(1, 2));
-		System.out.println(practice.add2(4, 6));
-}
-}
