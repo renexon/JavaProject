8de84f0d3d71e0dcdd6ba543e363748e61f95c13
“renexon” <mujahidul15@gmail.com>
2019-10-03 20:44:05 -0400

    added constructor

---
+++ src/Constructor.java
@@ -0,0 +1,8 @@
+
+public class Constructor {
+	public static void main (String[] args) {
+		Practice practice =new Practice();
+		System.out.println(practice.sub2(1, 2));
+		System.out.println(practice.add2(4, 6));
+}
+}
